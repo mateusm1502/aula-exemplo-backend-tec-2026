e8d198d8998c55bb0fde9dfc9e6c9f21b6ca612b
Mateus Moreira Pinto <mateusmp1502@gmail.com>
2026-07-02 22:10:48 -0300

    exemplo

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+// server ou app
+const app = express()
+
+app.get("/api/v1/somar", (req, res) => {
+    console.log(req.query)
+    const num1 = Number(req.query.num1)
+    const num2 = Number(req.query.num2)
+    const resultado = num1 + num2
+
+    res.send({ resultado })
+})
+
+app.listen(3000, () => {
+    console.log("Servidor ouvindo na porta 3000")
+})
