e1e8a1eaab1738ca44731c3322e82b64fd94eca1
Mateus Moreira Pinto <mateusmp1502@gmail.com>
2026-07-08 20:56:44 -0300

    lixo

--- repository/index.js
+++
@@ -1,8 +0,0 @@
-// variavel: um espaço na memoria 
-// um array sao varios espaços na memoria 
-const bancoDeDados = []
-bancoDeDados.push("joao")
-bancoDeDados.push("pedro")
-bancoDeDados.push("alberto")
-
-export default bancoDeDados
