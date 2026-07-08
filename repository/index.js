90135e077da852e23c2cd2dc532705d498c7c590
Mateus Moreira Pinto <mateusmp1502@gmail.com>
2026-07-08 19:08:41 -0300

    exemplo

---
+++ repository/index.js
@@ -0,0 +1,8 @@
+// variavel: um espaço na memoria 
+// um array sao varios espaços na memoria 
+const bancoDeDados = []
+bancoDeDados.push("joao")
+bancoDeDados.push("pedro")
+bancoDeDados.push("alberto")
+
+export default bancoDeDados
