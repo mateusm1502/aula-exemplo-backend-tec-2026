8f03fee6b8c966e6885b4e93d84b088a4f9a177d
Mateus Moreira Pinto <mateusmp1502@gmail.com>
2026-07-13 21:29:18 -0300

    aula-postman

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ import express from 'express'
 // server ou app
 const app = express()
 
+app.use(express.json())
+
 // somar 
-app.get("/api/v1/somar", ( req, res) => {
-    const { num1, num2 } = req.query
+app.post("/api/v1/somar", ( req, res) => {
+    const { num1, num2 } = req.body
     const resultado = Number(num1) + Number(num2)
     res.send({ message: resultado })
 })
 
 // subtrair
-app.get("/api/v1/subtrair", ( req, res ) => {
-    const { num1, num2 } = req.query
+app.post("/api/v1/subtrair", ( req, res ) => {
+    const { num1, num2 } = req.body
     const resultado = Number(num1) - Number(num2)
     res.send({ message: resultado })
 })
