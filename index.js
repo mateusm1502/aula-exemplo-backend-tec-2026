36cc38a45fda528a20f5a578853d6241aa87fc9f
Mateus Moreira Pinto <mateusmp1502@gmail.com>
2026-07-16 20:00:42 -0300

    exemplo

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 import express from 'express'
+import cachorro from './router/cachorro.js'
  
 // server ou app
 const app = express()
 
 app.use(express.json())
 
-// somar 
-app.post("/api/v1/somar", ( req, res) => {
-    const { num1, num2 } = req.body
-    const resultado = Number(num1) + Number(num2)
-    res.send({ message: resultado })
-})
+app.use('/api/v1/cachorro', cachorro)
 
-// subtrair
-app.post("/api/v1/subtrair", ( req, res ) => {
-    const { num1, num2 } = req.body
-    const resultado = Number(num1) - Number(num2)
-    res.send({ message: resultado })
-})
+// app.post("/api/v1/somar", ( req, res) => {
+//     const { num1, num2 } = req.body
+//     const resultado = Number(num1) + Number(num2)
+//     res.send({ message: resultado })
+// })
 
-// mult
-app.get("/api/v1/mult", ( req, res ) => {
-    const { num1, num2 } = req.query
-    const resultado = Number(num1) * Number(num2)
-    res.send({ message: resultado })
-} )
+// app.post("/api/v1/subtrair", ( req, res ) => {
+//     const { num1, num2 } = req.body
+//     const resultado = Number(num1) - Number(num2)
+//     res.send({ message: resultado })
+// })
 
-// div 
-app.get("/api/v1/div", ( req, res ) => {
-    const { num1, num2 } = req.query
-    const resultado = Number(num1) / Number(num2)
-    res.send({ message: resultado })
-})
+// app.get("/api/v1/mult", ( req, res ) => {
+//     const { num1, num2 } = req.query
+//     const resultado = Number(num1) * Number(num2)
+//     res.send({ message: resultado })
+// } )
+
+// app.get("/api/v1/div", ( req, res ) => {
+//     const { num1, num2 } = req.query
+//     const resultado = Number(num1) / Number(num2)
+//     res.send({ message: resultado })
+// })
 
 app.listen(3000,() => {
     console.log("Servidor ouvindo na porta 3000")
